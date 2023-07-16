2951f6db5adf5f99f408764a55e1938053e4c319
miketanuki <idyuuki999@gmail.com>
2023-07-16 17:55:08 +0900

    update:切り替わりのフェードを整理

--- src/components/Kimochi.tsx
+++ src/components/Kimochi.tsx
@@ -1,31 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
 import "../index.css";
 
 type KimochiProps = {
   averageScore: null | number;
 };
 
-/**
- * スコアに応じた気持ちの画像を表示するコンポーネント
- * @param averageScore 平均スコア
- */
 const SentimentImage: React.FC<KimochiProps> = ({ averageScore }) => {
-  const [opacity, setOpacity] = useState(0);
-  const [currentImage, setCurrentImage] = useState<null | string>(null);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+  const [nextImage, setNextImage] = useState<string | null>(null);
+  // const [testAvaregeScore, setTestAvaregeScore] = useState<number>(1);
 
   useEffect(() => {
-    setOpacity(0);
-  }, [currentImage]);
-
-  const handleImageLoad = () => {
-    setOpacity(1);
-  };
-
-  const getImage = (averageScore: null | number) => {
-    if (averageScore === null) {
-      return null;
-    }
     const images = [
+      { scoreRange: [0, 1], src: "kimochi/ame.svg", alt: "score 0-1" },
       { scoreRange: [0, 1], src: "kimochi/ame.svg", alt: "score 0-1" },
       { scoreRange: [1, 2], src: "kimochi/ame.svg", alt: "score 1-2" },
       { scoreRange: [2, 3], src: "kimochi/ame.svg", alt: "score 2-3" },
@@ -40,29 +28,48 @@ const SentimentImage: React.FC<KimochiProps> = ({ averageScore }) => {
 
     const image = images.find((image) => {
       const [min, max] = image.scoreRange;
-      return averageScore >= min && averageScore < max;
+      return averageScore !== null && averageScore >= min && averageScore < max;
     });
 
-    if (image) {
-      if (currentImage !== image.src) {
-        setCurrentImage(image.src);
-      }
-      return (
-        <img
-          src={currentImage || ""}
-          alt={image.alt}
-          style={{ opacity, transition: "0.5s ease-in-out" }}
-          onLoad={handleImageLoad}
-        />
-      );
-    } else {
-      return null;
+    if (image && currentImage !== image.src) {
+      setNextImage(image.src);
     }
+  }, [averageScore, currentImage]);
+
+  const handleImageLoad = () => {
+    setCurrentImage(nextImage);
+    setNextImage(null);
   };
 
+  // const handleButtonClick = () => {
+  //   setTestAvaregeScore(testAvaregeScore + 1);
+  // };
+
   return (
-    <div className="max-w-xl mx-auto mt-4 min-h-[300px] md:min-h-[500px]">
-      {getImage(averageScore)}
+    <div>
+      <div className="max-w-xl mx-auto mt-4 min-h-[300px] md:min-h-[500px]">
+        {currentImage && (
+          <CSSTransition
+            in={!nextImage}
+            timeout={1000}
+            classNames="fade"
+            unmountOnExit
+          >
+            <img src={currentImage} alt="Kimochi" />
+          </CSSTransition>
+        )}
+        {nextImage && (
+          <img
+            src={nextImage}
+            alt="Kimochi"
+            style={{ opacity: 0 }}
+            onLoad={handleImageLoad}
+          />
+        )}
+      </div>
+      {/* <div>
+      <button onClick={handleButtonClick}>Add 1 to testAvaregeScore</button>
+    </div> */}
     </div>
   );
 };
